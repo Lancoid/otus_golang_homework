4ac22ccf8c9631001e2e1de39fbb3a987f50786e
Rinsi <hotabich@mail.ru>
2022-06-15 00:16:29 +0700

    "HW2 is completed"

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,39 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	data := []rune(input)
+	result := ""
+
+	for key, char := range data {
+		if key == 0 && unicode.IsDigit(char) {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(char) && unicode.IsDigit(data[key-1]) {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(char) {
+			count := int(char - '0')
+
+			if count == 0 {
+				result = result[:len(result)-1]
+				continue
+			}
+
+			result += strings.Repeat(string(data[key-1]), count-1)
+			continue
+		}
+
+		result += string(char)
+	}
+
+	return result, nil
 }
